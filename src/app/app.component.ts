d3d6562df947945cc00ae6094084fa293e1c4f74
Alaketu Olamilekan Soliu <alaketuolamilekan02@gmail.com>
2023-01-12 22:13:27 +0100

    push paystack inkine implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
 import { Component } from '@angular/core';
-
+declare var PaystackPop: any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  Paystack = PaystackPop;
   title = 'paystack-test';
+
+  openStack() {
+    const paystack = new PaystackPop();
+    paystack.newTransaction({
+      key: 'pk_test_dc7c232fe6f9d7ff3eaeeda082074ef765244dbf', // Replace with your public key
+      reference: new Date().getTime().toString(),
+      email: 'ollie@yopmail.com',
+      amount: 200000, //Amount is in the country's lowest currency. E.g Kobo, so 20000 kobo = N200
+      onCancel: () => {
+        this.onClose();
+      },
+      onSuccess: (transaction: any) => {
+        this.onSuccess(transaction);
+      },
+    });
+   
+  
+    
+  }
+
+  onClose() {
+    
+  }
+
+  onSuccess(data: any) {
+    console.log('data', data)
+  }
 }
